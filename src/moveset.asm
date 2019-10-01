ccc30c88d369bdd63512bc0e488c09398e8e46e5
JSsixtyfour <50251105+JSsixtyfour@users.noreply.github.com>
2019-09-30 21:44:42 -0500

    Revert "Merge branch 'master' of https://github.com/JSsixtyfour/smashremix"
    This reverts commit 4fc7ce78b4a007806310a882fe841ec1b635c319, reversing
    changes made to 5a3485f1ca86c2c0ca6145ad678987334cf0fd0e.

--- src/moveset.asm
+++ src/moveset.asm
@@ -1,59 +1,376 @@
 // Moveset.asm (Fray)
 
-// This file adds support for reading moveset data from a pointer, and contains other moveset data
-// related constants and macros.
+// NOTE: THIS FILE IS TEMPORARY
 
-scope Moveset {
-    // @ Description
-    // by default, 0x0004(t2) is an offset added to the base moveset file address
-    // by default, a value of 0x80000000 is used to indicate no moveset data
-    // new functionality will treat any value greater than 0x80000000 as a pointer to moveset data
-    scope get_cmd_address_: {
-        OS.patch_start(0x63140, 0x800E7940)
-        j       get_cmd_address_
-        nop
-        origin  0x6316C
-        base    0x800E796C
-        _return:
-        OS.patch_end()
-        // s1 = player struct
-        // v0 = moveset data address
-        // t2 = action parameter struct
-        addiu   sp, sp,-0x0010              // allocate stack space
-        sw      t0, 0x0004(sp)              // store t0
-        sw      t1, 0x0008(sp)              // store t1
-        
-        _check_pointer:
-        lw      t0, 0x0004(t2)              // t0 = moveset data offset/pointer
-        bgez    t0, _end                    // skip if t0 is between 0x00000000 and 0x7FFFFFFF (offset)
-        nop
-        _check_none:
-        lui     t1, 0x8000                  // t1 = 0x80000000
-        beq     t0, t1, _end                // skip if t0 = 0x80000000 (no moveset data)
-        nop
-        or      v0, t0, r0                  // overwrite moveset data address
-       
-        _end:
-        sw      t0, 0x0004(sp)              // load t0
-        lw      t1, 0x0008(sp)              // load t1
-        addiu   sp, sp, 0x0010              // deallocate stack space
-        sw      v0, 0x08AC(s1)              // store pointer
-        sw      v0, 0x086C(s1)              // original line (store pointer)
-        j       _return                     // return
-        nop
-    }
+// This file adds support for reading moveset data from a pointer
 
-    // @ Description
-    // adds a GO_TO moveset command
-    macro GO_TO(address) {
-        dw 0x90000000                       // command
-        dw {address}                        // pointer
+// @ Description
+// by default, this parameter is an offset added to the base moveset file address
+// by default, a value of 0x80000000 is used to indicate no moveset data
+// new functionality will treat any value greater than 0x80000000 as a pointer to moveset data
+scope moveset_data_: {
+    // s1 = player struct
+    // v0 = moveset data address
+    // t2 = action parameters
+    addiu   sp, sp,-0x0010              // allocate stack space
+    sw      t0, 0x0004(sp)              // store t0
+    sw      t1, 0x0008(sp)              // store t1
+    _check_pointer:
+    lw      t0, 0x0004(t2)              // t0 = moveset data offset/pointer
+    bgez    t0, _end                    // skip if t0 is between 0x00000000 and 0x7FFFFFFF (offset)
+    nop
+    _check_none:
+    lui     t1, 0x8000                  // t1 = 0x80000000
+    beq     t0, t1, _end                // skip if t0 = 0x80000000 (no moveset data)
+    nop
+    or      v0, t0, r0                  // overwrite moveset data address
+   
+    _end:      
+    sw      t0, 0x0004(sp)              // load t0
+    lw      t1, 0x0008(sp)              // load t1
+    addiu   sp, sp, 0x0010              // deallocate stack space
+    sw      v0, 0x08AC(s1)              // store pointer
+    sw      v0, 0x086C(s1)              // original line (store pointer)
+    j       _moveset_data_return        // return
+    nop
+    
+    
+    // write changes to rom
+    // moveset_data_ hook
+    pushvar origin, base
+    origin  0x63140
+    base    0x800E7940
+    j       moveset_data_
+    nop
+    origin  0x6316C
+    base    0x800E796C
+    _moveset_data_return:
+    
+    
+    // replace moveset data
+    scope gnd {
+        // up tilt
+        origin  0x9D4BC
+        dw      GND_UTILT
+        // forward smash
+        origin  0x9D4DC
+        dw      0x0000064E                  // animation
+        dw      GND_FSMASH
+        dw      0x00000000                  // animation flags
+        origin  0x9D4F4
+        dw      0x0000064E                  // animation
+        dw      GND_FSMASH
+        dw      0x00000000                  // animation flags
+        origin  0x9D50C
+        dw      0x0000064E                  // animation
+        dw      GND_FSMASH
+        dw      0x00000000                  // animation flags
+        // up smash
+        origin  0x9D518
+        dw      0x00000854                  // animation
+        dw      GND_USMASH
+        dw      0x00000000                  // animation flags
+        // dair
+        origin  0x9D564
+        dw      GND_DAIR
+        // ground down special
+        origin  0x9D630 
+        dw      GND_DSP_GROUND
+        // down special flip
+        origin  0x9D63C
+        dw      GND_DSP_FLIP
+        // down special landing
+        origin  0x9D648   
+        dw      GND_DSP_LAND
+        // air down special
+        origin  0x9D654
+        dw      GND_DSP_AIR
+        // ground up special
+        origin  0x9D66C
+        dw      GND_USP_GROUND
+        // up special grab
+        origin  0x9D678
+        dw      GND_USP_GRAB
+        // up special release
+        origin  0x9D684
+        dw      GND_USP_RELEASE
+        // air up special
+        origin  0x9D690
+        dw      GND_USP_AIR  
+        // taunt
+        origin  0x9D440
+        dw      0x00000861                  // animation
+        dw      GND_TAUNT 
+		// down smash
+        origin  0x9D524
+		base	0x80121D24
+        dw 		0x00000855					// animation
+		dw      GND_DOWN_SMASH 
+		// fair
+		origin 0x0009D53C
+		base 0x80121D3C
+		dw 0x00000857
+		// nair
+		origin 0x0009D530
+		base 0x80121D30
+		dw 0x00000667
+		// neutral special ground
+		origin 0x0009D618
+		dw      GND_NEUTRAL_SPECIAL_GROUND
+		// neutral special air
+		origin 0x0009D624
+		dw      GND_NEUTRAL_SPECIAL_AIR
     }
     
-    // @ Description
-    // adds a THROW_DATA moveset command
-    macro THROW_DATA(address) {
-        dw 0x30000000                       // command
-        dw {address}                        // pointer
-    }   
+    scope falco {
+        // taunt
+        origin  0x94C60
+        dw      0x00000868                  // animation
+        dw      FALCO_TAUNT
+        // up tilt
+        origin  0x94CD8
+        dw      0x00000869                  // animation
+        dw      FALCO_UTILT
+        // forward smash
+        origin  0x94D14
+        dw      0x00000867                  // animation
+        dw      FALCO_FSMASH
+        // jab 1
+        origin  0x94C70
+        dw      FALCO_JAB_1
+        // jab 2
+        origin  0x94C7C
+        dw      FALCO_JAB_2
+        // dash attack
+        origin  0x94C88
+        dw      FALCO_DASH_ATTACK
+        // up smash
+        origin  0x94D3C
+        dw      FALCO_USMASH
+        // bair
+        origin  0x94D6C
+        dw      FALCO_BAIR
+        // uair
+        origin  0x94D78
+        dw      FALCO_UAIR
+        // dair
+        origin  0x94D84
+        dw      FALCO_DAIR
+        // ground neutral special
+        origin  0x94E10
+        dw      0x000002E9                  // animation
+        dw      FALCO_NSP_GROUND
+        // air neutral special
+        origin  0x94E1C
+        dw      0x000002E9                  // animation
+        dw      FALCO_NSP_AIR
+    }
+    
+    scope ylink {
+        // air up special
+        origin  0x9ACB8
+        dw      YLINK_USP_AIR
+        // ground up special
+        origin  0x9ACA0
+        dw      YLINK_USP_GROUND
+        // ground up special
+        origin  0x9ACAC
+        dw      YLINK_USP_GROUND_END
+        // dash attack
+        origin  0x9AB04
+        dw      0x000004B2                  // animation
+        dw      YLINK_DASH_ATTACK
+        // ylink fair
+        origin  0x9ABE0
+        dw      YLINK_FAIR
+        // ylink bair
+        origin  0x9ABEC
+        dw      YLINK_BAIR
+        // ylink dair
+        origin  0x9AC04
+        dw      YLINK_DAIR
+         // ylink taunt
+        origin  0x9AAE0
+        dw      0x000008A4
+        dw      YLINK_TAUNT
+    }
+    scope drm {
+        // drm neutral special ground
+        origin  0x92FCC
+        base    0x801177CC
+        dw      DRM_NSP_GROUND
+        // drm neutral special air
+        origin  0x92FD8
+        base    0x801177D8
+        dw      DRM_NSP_AIR
+        // drm down special ground
+        origin  0x92FFC
+        base    0x801177FC
+        dw      DRM_DSP_GROUND
+        // drm down special air
+        origin  0x93008
+        base    0x80117808
+        dw      DRM_DSP_AIR
+        // drm up special air
+        origin  0x92FF0
+        base    0x801177F0
+        dw      DRM_USP
+        // drm up special ground
+        origin  0x92FE4
+        base    0x801177E4
+        dw      DRM_USP
+        // drm down air
+        origin  0x92F50
+        base    0x80117750
+        dw      0x00000858                  // animation
+        dw      DRM_DAIR
+        dw      0x00000000                  // animation flags
+        // drm up smash
+        origin  0x92F0C
+        base    0x8011770C
+        dw      DRM_UP_SMASH
+        // drm fair
+        origin  0x92F2C
+        base    0x8011772C
+        dw      0x00000859                  // animation
+        dw      DRM_FAIR
+        // drm dash attack
+        origin  0x92E58
+        base    0x80117658
+        dw      DRM_DASH_ATTACK
+        // drm bair
+        origin  0x92F3C
+        base    0x8011773C
+        dw      DRM_BAIR
+        // drm uair
+        origin  0x92F48
+        base    0x80117748
+        dw      DRM_UAIR
+        // drm utilt
+        origin  0x92EAC
+        base    0x801176AC
+        dw      DRM_UTILT
+        // fsmash
+        origin  0x92ED0
+        base    0x801176D0
+        dw      DRM_FSMASHU
+        origin  0x92EDC
+        base    0x801176DC
+        dw      DRM_FSMASHMU
+        origin  0x92EE8
+        base    0x801176E8
+        dw      DRM_FSMASHM
+        origin  0x92EF4
+        base    0x801176F4
+        dw      DRM_FSMASHMD
+        origin  0x92F00
+        base    0x80117700
+        dw      DRM_FSMASHD
+        // ftilt
+        origin  0x92E64
+        base    0x80117664
+        dw      DRM_FTILTUP
+        origin  0x92E7C
+        base    0x8011767C
+        dw      DRM_FTILTMID
+        origin  0x92E94
+        base    0x80117694
+        dw      DRM_FTILTDOWN
+        // jab 1
+        origin  0x92E40
+        base    0x80117640
+        dw      DRM_NEUTRAL1
+        // jab 2
+        origin  0x92E4C
+        base    0x8011764C
+        dw      DRM_NEUTRAL2
+        origin  0x92FA8
+        base    0x801177A8
+        // jab 2
+        dw      DRM_NEUTRAL3
+        // dtilt
+        origin  0x92EC4
+        base    0x801176C4
+        dw      DRM_DTILT
+		// taunt
+        origin  0x92E30
+		base	0x80117630
+        dw 		0x0000085A					// animation
+		dw      DRM_TAUNT
+    }
+    pullvar base, origin
+    
+    
+    // insert files
+    insert  GND_UTILT,"moveset/gnd/UP_TILT.bin"
+    insert  GND_FSMASH,"moveset/gnd/FORWARD_SMASH.bin"
+    insert  GND_USMASH,"moveset/gnd/UP_SMASH.bin"
+    insert  GND_DAIR,"moveset/gnd/DOWN_AERIAL.bin"
+    insert  GND_DSP_GROUND,"moveset/gnd/DOWN_SPECIAL_GROUND.bin"
+    insert  GND_DSP_FLIP,"moveset/gnd/DOWN_SPECIAL_FLIP.bin"
+    insert  GND_DSP_LAND,"moveset/gnd/DOWN_SPECIAL_LANDING.bin"
+    insert  GND_DSP_AIR,"moveset/gnd/DOWN_SPECIAL_AIR.bin"
+    insert  GND_USP_GRAB,"moveset/gnd/UP_SPECIAL_GRAB.bin"
+    insert  GND_USP_RELEASE,"moveset/gnd/UP_SPECIAL_RELEASE.bin"
+    insert  GND_USP_THROWDATA,"moveset/gnd/UP_SPECIAL_THROW_DATA.bin"
+    insert  GND_TAUNT,"moveset/gnd/TAUNT.bin"
+    GND_USP_GROUND:
+    dw      0x30000000                      // throw data pointer command
+    dw      GND_USP_THROWDATA               // throw data pointer
+    insert  "moveset/gnd/UP_SPECIAL_GROUND.bin"
+    GND_USP_AIR:
+    dw      0x30000000                      // throw data pointer command
+    dw      GND_USP_THROWDATA               // throw data pointer
+    insert  "moveset/gnd/UP_SPECIAL_AIR.bin"
+	insert	GND_DOWN_SMASH,"moveset/gnd/DOWN_SMASH.bin"
+	insert 	GND_NEUTRAL_SPECIAL_GROUND,"moveset/gnd/NEUTRAL_SPECIAL_GROUND.bin"
+	insert 	GND_NEUTRAL_SPECIAL_AIR,"moveset/gnd/NEUTRAL_SPECIAL_AIR.bin"
+    
+    insert  FALCO_TAUNT,"moveset/falco/TAUNT.bin"
+    insert  FALCO_JAB_1,"moveset/falco/JAB_1.bin"
+    insert  FALCO_JAB_2,"moveset/falco/JAB_2.bin"
+    insert  FALCO_DASH_ATTACK,"moveset/falco/DASH_ATTACK.bin"
+    insert  FALCO_UTILT,"moveset/falco/UP_TILT.bin"
+    insert  FALCO_FSMASH,"moveset/falco/FORWARD_SMASH.bin"
+    insert  FALCO_USMASH,"moveset/falco/UP_SMASH.bin"
+    insert  FALCO_BAIR,"moveset/falco/BACK_AERIAL.bin"
+    insert  FALCO_UAIR,"moveset/falco/UP_AERIAL.bin"
+    insert  FALCO_DAIR,"moveset/falco/DOWN_AERIAL.bin"
+    insert  FALCO_NSP_AIR,"moveset/falco/NEUTRAL_SPECIAL_AIR.bin"
+    insert  FALCO_NSP_GROUND,"moveset/falco/NEUTRAL_SPECIAL_GROUND.bin"
+    
+    insert  YLINK_USP_AIR,"moveset/ylink/UP_SPECIAL_AIR.bin"
+    insert  YLINK_USP_GROUND,"moveset/ylink/UP_SPECIAL_GROUND.bin"
+    insert  YLINK_USP_GROUND_END,"moveset/ylink/UP_SPECIAL_GROUND_END.bin"
+    insert  YLINK_DASH_ATTACK, "moveset/ylink/DASH_ATTACK.bin"
+    insert  YLINK_FAIR,"moveset/ylink/FORWARD_AERIAL.bin"
+    insert  YLINK_BAIR,"moveset/ylink/BACK_AERIAL.bin"
+    insert  YLINK_DAIR,"moveset/ylink/DOWN_AERIAL.bin"
+    insert  YLINK_TAUNT,"moveset/ylink/TAUNT.bin"
+    
+    insert  DRM_NSP_GROUND, "moveset/drm/NEUTRAL_SPECIAL_GROUND.bin"
+    insert  DRM_NSP_AIR, "moveset/drm/NEUTRAL_SPECIAL_AIR.bin"
+    insert  DRM_DSP_GROUND, "moveset/drm/DOWN_SPECIAL_GROUND.bin"
+    insert  DRM_DSP_AIR, "moveset/drm/DOWN_SPECIAL_AIR.bin"
+    insert  DRM_USP, "moveset/drm/UP_SPECIAL.bin"
+    insert  DRM_DAIR, "moveset/drm/DAIR.bin"
+    insert  DRM_FAIR, "moveset/drm/FAIR.bin"
+    insert  DRM_BAIR, "moveset/drm/BAIR.bin"
+    insert  DRM_UAIR, "moveset/drm/UAIR.bin"
+    insert  DRM_UP_SMASH, "moveset/drm/UP_SMASH.bin"
+    insert  DRM_DASH_ATTACK, "moveset/drm/DASH_ATTACK.bin"
+    insert  DRM_UTILT, "moveset/drm/UP_TILT.bin"
+    insert  DRM_FSMASHD, "moveset/drm/FSMASHDOWN.bin"
+    insert  DRM_FSMASHMD, "moveset/drm/FSMASHMIDDOWN.bin"
+    insert  DRM_FSMASHM, "moveset/drm/FSMASHMID.bin"
+    insert  DRM_FSMASHMU, "moveset/drm/FSMASHMIDUP.bin"
+    insert  DRM_FSMASHU, "moveset/drm/FSMASHUP.bin"
+    insert  DRM_FTILTDOWN, "moveset/drm/FTILTDOWN.bin"
+    insert  DRM_FTILTMID, "moveset/drm/FTILTMID.bin"
+    insert  DRM_FTILTUP, "moveset/drm/FTILTUP.bin"
+    insert  DRM_NEUTRAL1, "moveset/drm/NEUTRAL1.bin"
+    insert  DRM_NEUTRAL2, "moveset/drm/NEUTRAL2.bin"
+    insert  DRM_NEUTRAL3, "moveset/drm/NEUTRAL3.bin"
+    insert  DRM_DTILT, "moveset/drm/DTILT.bin"
+	insert  DRM_TAUNT, "moveset/drm/TAUNT.bin"
 }
---
+++ src/slowattack.asm
@@ -0,0 +1,372 @@
+// SlowAttack.asm (JS)
+
+// Sets the frame speed multiplier for an entire action. TEMPORARY.
+
+scope SlowAttack {
+    _slow_attack:
+    addiu   sp, sp,-0x0014                  // allocate stack space
+    sw      t0, 0x0004(sp)                  // ~
+    sw      t1, 0x0008(sp)                  // ~
+    sw      t2, 0x000C(sp)                  // store t0 - t2
+    swc1      f1, 0x0010(sp)                // store f1
+
+    //charactercheck
+    lw t2, 0x0084(a0)                       // load player struct address into t2
+    lhu t0, 0x000A(t2)                      // load player struct value of current player character
+    addiu t1, r0, 0x0007                    // load falconvalue into t1
+    beq t1, t0, _falconupsmashcheck         // if current player is captain falcon goto next check
+    nop
+    addiu t1, r0, 0x0005
+    beq t1, t0, _linkdashcheck
+    nop
+    addiu t1, r0, 0x0001
+    beq t1, t0, _foxruncheck
+    nop
+
+    j _end                                  // jump to end
+    nop
+
+    // falcon attack check
+
+    _falconupsmashcheck:
+    addiu t1, r0, 0x00CF                            // load the value of an upsmash to t1
+    beq t1, a1, _falconupsmash                      // if current action = upsmash, jump to upsmash
+    nop
+
+    _falcondashcheck:
+    addiu t1, r0, 0x00C0                            // load the value of a dash to t1
+    beq t1, a1, _falcondash                         // if current action = upsmash, jump to upsmash
+    nop
+
+    _falconforwardtiltupcheck:
+    addiu t1, r0, 0x00C1                            // load the value of a forward tilt to t1
+    beq t1, a1, _falconforwardtiltup                // if current action = upsmash, jump to upsmash
+    nop
+
+    _falconforwardtiltmidupcheck:
+    addiu t1, r0, 0x00C2                            // load the value of an upsmash to t1
+    beq t1, a1, _falconforwardtiltmidup             // if current action = upsmash, jump to upsmash
+    nop
+
+    _falconforwardtiltsidecheck:
+    addiu t1, r0, 0x00C3                            // load the value of an upsmash to t1
+    beq t1, a1, _falconforwardtiltside              // if current action = upsmash, jump to upsmash
+    nop
+
+    _falconforwardtiltmiddowncheck:
+    addiu t1, r0, 0x00C4                            // load the value of an upsmash to t1
+    beq t1, a1, _falconforwardtiltmiddown           // if current action = upsmash, jump to upsmash
+    nop
+
+    _falconforwardtiltdowncheck:
+    addiu t1, r0, 0x00C5                            // load the value of an upsmash to t1
+    beq t1, a1, _falconforwardtiltdown              // if current action = upsmash, jump to upsmash
+    nop
+
+    _falconnaircheck:
+    addiu t1, r0, 0x00D1                            // load the value of an upsmash to t1
+    beq t1, a1, _falconnair                         // if current action = upsmash, jump to upsmash
+    nop
+
+    _falconfaircheck:
+    addiu t1, r0, 0x00D2                            // load the value of an upsmash to t1
+    beq t1, a1, _falconfair                         // if current action = upsmash, jump to upsmash
+    nop
+
+    _falconuaircheck:
+    addiu t1, r0, 0x00D4                            // load the value of an upsmash to t1
+    beq t1, a1, _falconuair                         // if current action = upsmash, jump to upsmash
+    nop
+
+    _falconruncheck:
+    addiu t1, r0, 0x0010                            // load the value of dash to t1
+    beq t1, a1, _falconrun                          // if current action = falconrun, jump to upsmash
+    nop
+
+    _falconjabcheck:
+    addiu t1, r0, 0x00BE                            // load the value of jab to t1
+    beq t1, a1, _falconjab                          // if current action = jab, jump to jab
+    nop
+
+    _falconpunchcheck:
+    addiu t1, r0, 0x00E4                            // load the value of jab to t1
+    beq t1, a1, _falconpunch                            // if current action = jab, jump to jab
+    nop
+
+    _falconpunchaircheck:
+    addiu t1, r0, 0x00E5                            // load the value of jab to t1
+    beq t1, a1, _falconpunchair                         // if current action = jab, jump to jab
+    nop
+
+    _falconidlecheck:
+    addiu t1, r0, 0x000A                            // load the value of jab to t1
+    beq t1, a1, _falconidle                         // if current action = jab, jump to jab
+    nop
+
+    //end of falcon attack checks
+    j _end                                  // jump to end because no falcon attacks need to be slowed
+    nop
+
+    //falconattackslowdown
+
+    _falconpunch:
+    lui a3, 0x3F66
+    addiu a3, a3, 0x6666                    // set to 90%
+    j _end                                  // jump to end
+    nop
+
+    _falconpunchair:
+    lui a3, 0x3F66
+    addiu a3, a3, 0x6666                    // set to 90%
+    j _end                                  // jump to end
+    nop
+
+    _falconupsmash:
+    lui a3, 0x3F80
+    addiu a3, a3, 0x0000                    // set to 100%
+    j _end                                  // jump to end
+    nop
+
+    _falcondash:
+    lui a3, 0x3F66
+    addiu a3, a3, 0x6666                    // set to 90%
+    j _end                          // jump to end
+    nop
+
+    _falconforwardtiltup:
+    lui a3, 0x3F66
+    addiu a3, a3, 0x6666                    // set to 90%
+    j _end                          // jump to end
+    nop
+
+    _falconforwardtiltmidup:
+    lui a3, 0x3F66
+    addiu a3, a3, 0x6666                    // set to 90%
+    j _end                          // jump to end
+    nop
+
+    _falconforwardtiltside:
+    lui a3, 0x3F66
+    addiu a3, a3, 0x6666                    // set to 90%
+    j _end                          // jump to end
+    nop
+
+    _falconforwardtiltmiddown:
+    lui a3, 0x3F66
+    addiu a3, a3, 0x6666                    // set to 90%
+    j _end                          // jump to end
+    nop
+
+    _falconforwardtiltdown:
+    lui a3, 0x3F66
+    addiu a3, a3, 0x6666            // set to 90%
+    j _end                          // jump to end
+    nop
+
+    _falconnair:
+    lui a3, 0x3f67                  // set to 90ish%
+    j _end                          // jump to end
+    nop
+
+    _falconfair:
+    lui a3, 0x3FA7                  // set to 130ish%
+    j _end                          // jump to end
+    nop
+
+    _falconuair:
+    lui a3, 0x3F4C
+    addiu a3, a3, 0xCCCD            // set to 80%
+    j _end                          // jump to end
+    nop
+
+    _falconrun:
+    lui a3, 0x3f0D                  // set to 55ish%
+    j _end                                  // jump to end
+    nop
+
+    _falconjab:
+    lui a3, 0x3F8C
+    addiu a3, a3, 0xCCCD            // set to 110%
+    j _end                          // jump to end
+    nop
+
+    _falconidle:
+    lui a3, 0x3f33                  // set to 70ish%
+    j _end                                  // jump to end
+    nop
+
+    // link attack checks
+    _linkdashcheck:
+    addiu t1, r0, 0x00C0                    // load the value of an attack to t1
+    beq t1, a1, _linkdash                   // if current action = attack, jump to attack
+    nop
+
+    _linkftiltcheck:
+    addiu t1, r0, 0x00C3                    // load the value of an attack to t1
+    beq t1, a1, _linkftilt                  // if current action = attack, jump to attack
+    nop
+
+    _linkfsmashcheck:
+    addiu t1, r0, 0x00CC                    // load the value of an attack to t1
+    beq t1, a1, _linkfsmash                 // if current action = attack, jump to attack
+    nop
+
+    _linkgrabcheck:
+    addiu t1, r0, 0x00A6                    // load the value of an attack to t1
+    beq t1, a1, _linkgrab                   // if current action = attack, jump to attack
+    nop
+
+    _linkutiltcheck:
+    addiu t1, r0, 0x00C7                    // load the value of an attack to t1
+    beq t1, a1, _linkutilt                  // if current action = attack, jump to attack
+    nop
+
+    _linkdtiltcheck:
+    addiu t1, r0, 0x00C9                    // load the value of an attack to t1
+    beq t1, a1, _linkdtilt                  // if current action = attack, jump to attack
+    nop
+
+    _linkusmashcheck:
+    addiu t1, r0, 0x00CF                    // load the value of an attack to t1
+    beq t1, a1, _linkusmash                 // if current action = attack, jump to attack
+    nop
+
+    _linkdsmashcheck:
+    addiu t1, r0, 0x00D0                    // load the value of an attack to t1
+    beq t1, a1, _linkdsmash                 // if current action = attack, jump to attack
+    nop
+
+    _linkuaircheck:
+    addiu t1, r0, 0x00D4                    // load the value of an attack to t1
+    beq t1, a1, _linkuair                   // if current action = attack, jump to attack
+    nop
+
+    _linkdaircheck:
+    addiu t1, r0, 0x00D5                    // load the value of an attack to t1
+    beq t1, a1, _linkdair                   // if current action = attack, jump to attack
+    nop
+
+    _linkbombcheck:
+    addiu t1, r0, 0x00E8                    // load the value of an attack to t1
+    beq t1, a1, _linkbomb                   // if current action = attack, jump to attack
+    nop
+
+    _linkboomerangcheck:
+    addiu t1, r0, 0x00E5                    // load the value of an attack to t1
+    beq t1, a1, _linkboomerang              // if current action = attack, jump to attack
+    nop
+
+    //end of link attack checks
+    j _end                                  // jump to end because no link attacks need to be slowed
+    nop
+
+    //link speed changes
+
+    _linkdash:
+    lui a3, 0x3F8C
+    addiu a3, a3, 0xCCCD                    // set to 110%
+    j _end                                  // jump to end
+    nop
+
+    _linkftilt:
+    lui a3, 0x3f99
+    addiu a3, a3, 0x999A                    // set to 120%
+    j _end                                  // jump to end
+    nop
+
+    _linkutilt:
+    lui a3, 0x3F86
+    addiu a3, a3, 0x6666                    // set to 105%
+    j _end                                  // jump to end
+    nop
+
+    _linkdtilt:
+    lui a3, 0x3F93
+    addiu a3, a3, 0x3333                    // set to 115%
+    j _end                                  // jump to end
+    nop
+
+    _linkusmash:
+    lui a3, 0x3FA6
+    addiu a3, a3, 0x6666                    // set to 130%
+    j _end                                  // jump to end
+    nop
+
+    _linkfsmash:
+    lui a3, 0x3f99
+    addiu a3, a3, 0x999A                    // set to 120%
+    j _end                                  // jump to end
+    nop
+
+    _linkdsmash:
+    lui a3, 0x3F86
+    addiu a3, a3, 0x6666                    // set to 105%
+    j _end                                  // jump to end
+    nop
+
+    _linkuair:
+    lui a3, 0x3F8C
+    addiu a3, a3, 0xCCCD                    // set to 110%
+    j _end                                  // jump to end
+    nop
+
+    _linkdair:
+    lui a3, 0x3F86
+    addiu a3, a3, 0x6666                    // set to 105%
+    j _end                                  // jump to end
+    nop
+
+    _linkboomerang:
+    lui a3, 0x3F86
+    addiu a3, a3, 0x6666                    // set to 105%
+    j _end                                  // jump to end
+    nop
+
+    _linkbomb:
+    lui a3, 0x3f80                          // set to 100%
+    j _end                                  // jump to end
+    nop
+
+    _linkgrab:
+    lui a3, 0x3f99
+    addiu a3, a3, 0x999A                    // set to 120%
+    j _end                                  // jump to end
+    nop
+
+    // fox attack checks
+
+    _foxruncheck:
+    addiu t1, r0, 0x0010                    // load the value of an attack to t1
+    beq t1, a1, _foxrun                     // if current action = attack, jump to attack
+    nop
+
+    _foxdashcheck:
+    addiu t1, r0, 0x000F                    // load the value of an attack to t1
+    beq t1, a1, _foxdash                    // if current action = attack, jump to attack
+    nop
+
+    j _end                                  // jump to end because attack checks are over
+    nop
+
+    // fox speed changes
+
+    _foxrun:
+    lui a3, 0x3F40
+    j _end                                  // jump to end
+    nop
+
+    _foxdash:
+    lui a3, 0x3F58
+    j _end                                  // jump to end
+    nop
+
+    // ending
+    _end:
+    lw      t0, 0x0004(sp)                  // ~
+    lw      t1, 0x0008(sp)                  // ~
+    lw      t2, 0x000C(sp)                  // ~ restore value of t0 - t2
+    lwc1      f1, 0x0010(sp)                // ~ restore value of f1
+    addiu   sp, sp, 0x0014                  //reallocate stack space
+    j       Command.change_action_._slow_attack_return             
+    nop
+}
