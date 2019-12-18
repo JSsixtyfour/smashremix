74d00a25ef9415015dacbff6b74a43dd97396bd5
John Liljegren <jtl3d@virginia.edu>
2019-12-18 16:43:35 -0500

    Implemented costume switching on training CSS  - TODO: fix shade_id - not saved when leaving CSS currently

--- src/Costumes.asm
+++ src/Costumes.asm
@@ -4,7 +4,8 @@ define __COSTUMES__()
 print "included Costumes.asm\n"
 
 // @ Description
-// This file makes all SSB costumes usable in versus.
+// This file makes all SSB costumes usable in versus and training.
+// TODO: Shades don't work in Training - once you leave the CSS, the shade resets to default
 
 include "OS.asm"
 
@@ -16,16 +17,22 @@ scope Costumes {
     // @ Warning
     // This is not a function that should be called. It is a patch/hook. 
     scope select_: {
-        OS.patch_start(0x0001361B4, 0x80137F34)
-        j       Costumes.select_
+        // vs
+        OS.patch_start(0x001361B4, 0x80137F34)
+        jal     Costumes.select_
         nop
-        _select_return:
+        OS.patch_end()
+
+        // training
+        OS.patch_start(0x00144BF8, 0x80135618)
+        jal     Costumes.select_
+        nop
         OS.patch_end()
 
         // a0 holds character id (until original line 2)
         // a1 holds direction pressed (up = 0, right = 1, down = 2, left = 3)
 
-        addiu   sp, sp,-0x0028              // allocate stack sapce
+        addiu   sp, sp,-0x0028              // allocate stack space
         sw      t0, 0x0004(sp)              // ~
         sw      t1, 0x0008(sp)              // ~
         sw      t2, 0x000C(sp)              // ~
@@ -39,13 +46,30 @@ scope Costumes {
         sll     t1, a1, 0x0002              // t1 = action * 4 = offset
         add     t0, t0, t1                  // t0 = functions + offset
         lw      t0, 0x0000(t0)              // t0 = function
-        li      t1, num_costumes            // t0 = num_costumes Table
-        add     t1, t1, a0                  // t0 = num_costumes + offset
-        lb      t1, 0x0000(t1)              // t0 = number of costumes char has
-        lw      t2, 0x0050(s0)              // a0 = current shade_id
+        li      t1, num_costumes            // t1 = num_costumes Table
+        add     t1, t1, a0                  // t1 = num_costumes + offset
+        lb      t1, 0x0000(t1)              // t1 = number of costumes char has
+
+        li      at, 0x80135620              // at = ra for training
+        beq     at, ra, _training           // if we're in training, then skip next part
+        nop
+        lw      t2, 0x0050(s0)              // t2 = current shade_id
+        b       _go_to_function
+        nop
+
+        _training:
+        li      at, Global.p_struct_head    // Global player struct head
+        lw      at, 0x0000(at)              // 1st loaded player struct
+        li      v0, Global.P_STRUCT_LENGTH  // v0 = Global.P_STRUCT_LENGTH
+        lb      t2, 0x000D(at)              // t2 = 0 if human, 1 if CPU
+        multu   t2, v0                      // Get offset of human player (human will be loaded 2nd sometimes)
+        mflo    t2                          // t2 = offset
+        addu    at, at, t2                  // at = human player struct
+        lb      t2, 0x0011(at)              // t2 = current shade_id
+
+        _go_to_function:
         jr      t0                          // go to function
         lw      v0, 0x004C(s0)              // v0 = current costume_id          
-
 
         // change costume
         _right:
@@ -74,7 +98,6 @@ scope Costumes {
         b       _end                        // end
         nop
 
-
         _down:
         bgtz    t2, _end                    // if (costume_id =)
         addiu   t2, t2,-0x0001              // then, t2--
@@ -82,15 +105,20 @@ scope Costumes {
         b       _end                        // end
         nop
 
-
         _end:
         sw      v0, 0x004C(s0)              // store updated costume_id
-        sw      t2, 0x0050(s0)              // store updated shade_id       
-
         lw      a0, 0x0008(s0)              // a0 = (?)
         lw      a1, 0x004C(s0)              // a1 = costume_id
-        lw      a2, 0x0050(s0)              // a2 = shade_id
 
+        li      at, 0x80135620              // at = ra for training
+        beq     at, ra, _training_2         // if we're in training, then skip next part
+        nop
+        sw      t2, 0x0050(s0)              // store updated shade_id
+
+        _training_2:
+        or        a2, t2, r0                // a2 = shade_id
+
+        _finish:
         addiu   sp, sp,-0x0008              // allocate stack space
         sw      v0, 0x0004(sp)              // save v0
         jal     Costumes.update_            // apply costume
@@ -106,12 +134,12 @@ scope Costumes {
         lw      a1, 0x0018(sp)              // ~
         lw      a2, 0x001C(sp)              // ~
         lw      ra, 0x0020(sp)              // restore registers
-        addiu   sp, sp, 0x0028              // deallocate stack sapce
+        addiu   sp, sp, 0x0028              // deallocate stack space
         
         sw      v0, 0x0024(sp)              // original line 1
         lw      a0, 0x0048(s0)              // original line 2  
 
-        j       _select_return              // return
+        jr      ra                          // return
         nop
 
         num_costumes:
@@ -162,10 +190,16 @@ scope Costumes {
     // @ Description
     // This is a bug fix to prevent the costume written to using CLeft and CRight from being updated.
     scope disable_update: {
+        // vs
         OS.patch_start(0x001361EC, 0x80137F6C)
         nop                                 // sw v0, 0x0050(s0)
         lw      a0, 0x0008(s0)              // original line 2
         lw      a1, 0x0024(sp)              // original line 3
+        nop                                 // jal update_
+        OS.patch_end()
+
+        // training
+        OS.patch_start(0x00144C28, 0x80135648)
         nop                                 // jal update_
         OS.patch_end()
     }
