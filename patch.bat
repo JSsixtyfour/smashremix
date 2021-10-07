87a867393a6bfdc7b5939dd35f499292a8a61f32
goombapatrol <toombapatrol@gmail.com>
2021-10-07 12:45:43 -0500

    bat files work relatively, pausing only if clicked

--- patch.bat
+++ patch.bat
@@ -1,4 +1,8 @@
+@echo off
+echo %cmdcmdline%|find /i """%~f0""">nul && set isclick=true
+pushd %~dp0
 assembler\bass.exe -o "ssb64asm.z64" main.asm -sym logfile.log
 assembler\chksum64.exe "ssb64asm.z64" > nul
 assembler\rn64crc.exe -u > nul
-pause
+popd
+if ["%isclick%"]==["true"] pause
--- xdelta - apply original.bat
+++ xdelta - apply original.bat
@@ -1,2 +1,6 @@
+@echo off
+echo %cmdcmdline%|find /i """%~f0""">nul && set isclick=true
+pushd %~dp0
 xdelta -d -f -s "roms\ssb.rom" original.xdelta roms\original.z64
-pause
+popd
+if ["%isclick%"]==["true"] pause
