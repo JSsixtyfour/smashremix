4008d5cf8f4e37227f07394c8bc3cf1f18bead90
goombapatrol <toombapatrol@gmail.com>
2021-10-07 16:11:02 -0500

    minor echo adjustment

--- patch.bat
+++ patch.bat
@@ -1,8 +1,7 @@
-@echo off
-echo %cmdcmdline%|find /i """%~f0""">nul && set isclick=true
-pushd %~dp0
+@echo %cmdcmdline%|find /i """%~f0""">nul && set isclick=true
+@pushd %~dp0
 assembler\bass.exe -o "ssb64asm.z64" main.asm -sym logfile.log
-assembler\chksum64.exe "ssb64asm.z64" > nul
-assembler\rn64crc.exe -u > nul
-popd
-if ["%isclick%"]==["true"] pause
+@assembler\chksum64.exe "ssb64asm.z64" > nul
+@assembler\rn64crc.exe -u > nul
+@popd
+@if ["%isclick%"]==["true"] pause
--- xdelta - apply original.bat
+++ xdelta - apply original.bat
@@ -1,6 +1,5 @@
-@echo off
-echo %cmdcmdline%|find /i """%~f0""">nul && set isclick=true
-pushd %~dp0
+@echo %cmdcmdline%|find /i """%~f0""">nul && set isclick=true
+@pushd %~dp0
 xdelta -d -f -s "roms\ssb.rom" original.xdelta roms\original.z64
-popd
-if ["%isclick%"]==["true"] pause
+@popd
+@if ["%isclick%"]==["true"] pause
